537c03e08425308cb246259bdb36c10169d9940e
dawa192apple <dawa192apple@192.168.100.5>
2017-07-07 13:14:11 +0545

    new updates

--- javaTutorial/HelloSpringMvc/src/main/java/com/optimystic/login/LoginService.java
+++ javaTutorial/HelloSpringMvc/src/main/java/com/optimystic/login/LoginService.java
@@ -1,11 +1,37 @@
 package com.optimystic.login;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
 import org.springframework.stereotype.Service;
+
+import com.mysql.jdbc.PreparedStatement;
 
 @Service
 public class LoginService {
 	public Boolean loginValidation(String username, String password ){
-		return username.equals("dawa") && password.equals("lama");
+		String url="jdbc:mysql://localhost:3306/spring";
+		String u="root";
+		String p="";
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url,u,p);
+			PreparedStatement ps = (PreparedStatement) con.prepareStatement("select * from login where username=? and password=?");
+			ps.setString(1, username);
+			ps.setString(2, password);
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()){
+				return true;
+			}
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		return false;
+		//return username.equals("dawa") && password.equals("lama");
 	}
 
 }
--- javaTutorial/HelloSpringMvc/src/main/webapp/WEB-INF/views/login.jsp
+++ javaTutorial/HelloSpringMvc/src/main/webapp/WEB-INF/views/login.jsp
@@ -5,7 +5,9 @@
 <body>
     <p><font color="red">${errorMessage}</font></p>
     <form action="/login" method="POST">
-        Name : <input name="name" type="text" /> Password : <input name="password" type="password" /> <input type="submit" />
+        Name : <input name="name" type="text" /> 
+        Password : <input name="password" type="password" /> 
+        <input type="submit" />
     </form>
 </body>
 </html>
